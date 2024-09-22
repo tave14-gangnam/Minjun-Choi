cd4fde1851312e6c74c989cca60228d892a9b435
Choi Minjune <minjune0807@gmail.com>
2024-09-23 05:11:33 +0900

    [init] 초기설정

---
+++ src/main/java/com/example/tave/TaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.tave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tave/TaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
